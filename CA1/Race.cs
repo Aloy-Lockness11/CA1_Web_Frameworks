9e896d1b58e57fb7b2d0a5eef534377412ef6d93
Aloysius <aloysiuspacheco2003@gmail.com>
2024-10-20 21:56:45 +0100

    Add project files.

---
+++ CA1/Race.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    internal class Race:Event
+    {
+        private String locations;
+        public List<string> horses;
+
+        
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Location
+        {
+            get { return locations; }
+            set { locations = value; }
+        }
+
+        public Race(string name, DateTime date, TimeOnly duration,String locations) : base(name, date, duration)
+        {
+
+        }
+
+        public bool menu()
+        {
+            return false;
+        }
+
+
+    }
+}
---
+++ CA1/RaceCourseManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    internal class RaceCourseManager : User
+    {
+        private List<RaceDayEvent> raceEventList;
+
+        public RaceCourseManager(string name, string id, string email, string username, string password) : base(name, id, email, username, password)
+        {
+        }
+
+        public override bool menu()
+        {
+            throw new NotImplementedException();
+        }
+    }  
+}
